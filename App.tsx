396fe5a2b2f1e763889236fd04bd95435496979f
WildPastry <mykdsn@gmail.com>
2023-02-27 17:42:26 +1300

    Adjust scripts. Bump versions.

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>PUFF FREE</Text>
+      <Text style={styles.title}>PUFF NO MORE</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#B33771',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    color: '#F8EFBA',
+    fontSize: 30
   },
 });
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# PuffFree
+# PuffNoMore
 
 React Native
