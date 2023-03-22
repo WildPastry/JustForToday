f68ec5d02cbf0d7b1f9220495d7df3e9768c6f65
WildPastry <mykdsn@gmail.com>
2023-03-22 19:48:12 +1300

    Add linting and styling. Add scripts.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Just for today</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -15,10 +15,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#227093',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   title: {
     color: '#f7f1e3',
     fontSize: 25
-  },
+  }
 });
