77085caefd8c72ab4116630a96a3c55b1d8fba0a
WildPastry <mykdsn@gmail.com>
2023-03-25 17:02:32 +1300

    Finish basic test case

--- app/screens/home/Home.spec.tsx
+++ app/screens/home/Home.spec.tsx
@@ -1,12 +1,10 @@
 import Home from './Home';
-import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react-native';
 
 describe('<Home />', () => {
   it('renders the component', () => {
-    // const container = render(<Home />);
-    // expect(container).toBeTruthy();
-    const tree = renderer.create(<Home />).toJSON();
-    expect(tree).toMatchSnapshot();
+    const container = render(<Home />);
+    expect(container).toMatchSnapshot();
+    expect(container).toBeTruthy();
   });
 });
