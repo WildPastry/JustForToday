5ba8eaec6785140937a0ac95a7b5cf10bd864515
WildPastry <mykdsn@gmail.com>
2023-04-01 07:58:30 +1300

    Update cached resources

--- app/hooks/useCachedResources.ts
+++ app/hooks/useCachedResources.ts
@@ -1,7 +1,7 @@
-import { FontAwesome } from '@expo/vector-icons';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect, useState } from 'react';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function useCachedResources() {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
@@ -19,7 +19,6 @@ export default function useCachedResources() {
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
-        console.warn(e);
       } finally {
         setLoadingComplete(true);
         SplashScreen.hideAsync();
