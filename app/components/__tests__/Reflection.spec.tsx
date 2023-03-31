896a8452296865bd9c904458bf5c712293eeb642
WildPastry <mykdsn@gmail.com>
2023-04-01 08:11:16 +1300

    Updating tests

--- app/components/__tests__/Reflection.spec.tsx
+++ app/components/__tests__/Reflection.spec.tsx
@@ -1,9 +1,18 @@
+import { Provider } from 'react-redux';
 import Reflection from '../Reflection';
+import { makeStore } from '../../redux/store';
 import renderer from 'react-test-renderer';
 
 describe('<Reflection />', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(<Reflection />).toJSON();
+    const store = makeStore();
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Reflection />
+        </Provider>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
--- app/screens/__tests__/Homes.spec.tsx
+++ app/screens/__tests__/Homes.spec.tsx
@@ -1,9 +1,18 @@
 import Home from '../Home';
+import { Provider } from 'react-redux';
+import { makeStore } from '../../redux/store';
 import renderer from 'react-test-renderer';
 
 describe('<Home />', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(<Home />).toJSON();
+    const store = makeStore();
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Home />
+        </Provider>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
