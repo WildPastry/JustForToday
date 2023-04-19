8575438a06e478bca30b42baa282bc1d1077e88c
WildPastry <mykdsn@gmail.com>
2023-04-19 20:00:17 +1200

    Remove data for now

--- app/screens/__tests__/Home.spec.tsx
+++ app/screens/__tests__/Home.spec.tsx
@@ -4,14 +4,11 @@ import { Provider } from 'react-redux';
 import React from 'react';
 import { makeStore } from '../../redux/store';
 import { render } from '@testing-library/react-native';
-import dataSlice, { setData } from '../../redux/slices/dataSlice';
 
 describe('<Home />', () => {
   it('renders correctly with data', () => {
     // Create test store and data
     const store = makeStore();
-
-    jest.mock('data');
 
     render(
       <Provider store={store}>
@@ -20,6 +17,5 @@ describe('<Home />', () => {
         </NavigationContainer>
       </Provider>
     );
-
   });
 });
