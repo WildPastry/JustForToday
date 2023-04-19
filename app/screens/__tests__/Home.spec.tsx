454d27d0b7cc27d6c41f9d270a37eb008bd1a18e
WildPastry <mykdsn@gmail.com>
2023-04-19 19:45:34 +1200

    Updating tests

--- app/screens/__tests__/Home.spec.tsx
+++ app/screens/__tests__/Home.spec.tsx
@@ -1,18 +1,25 @@
 import Home from '../Home';
+import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
+import React from 'react';
 import { makeStore } from '../../redux/store';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react-native';
+import dataSlice, { setData } from '../../redux/slices/dataSlice';
 
 describe('<Home />', () => {
-  it('renders correctly', () => {
+  it('renders correctly with data', () => {
+    // Create test store and data
     const store = makeStore();
-    const tree = renderer
-      .create(
-        <Provider store={store}>
+
+    jest.mock('data');
+
+    render(
+      <Provider store={store}>
+        <NavigationContainer>
           <Home />
-        </Provider>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+        </NavigationContainer>
+      </Provider>
+    );
+
   });
 });
