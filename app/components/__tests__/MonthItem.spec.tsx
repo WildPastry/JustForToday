af689ca40fff7297efd728a22718547a888532ee
WildPastry <mparkernzdev@gmail.com>
2023-06-03 19:06:55 +1200

    Update monthitem test

--- app/components/__tests__/MonthItem.spec.tsx
+++ app/components/__tests__/MonthItem.spec.tsx
@@ -1,10 +1,17 @@
 import MonthItem from '../layout/MonthItem';
+import { Provider } from 'react-redux';
+import { makeStore } from '../../redux/store';
 import renderer from 'react-test-renderer';
 
 describe('<MonthItem />', () => {
   it('renders correctly', () => {
+    const store = makeStore();
     const tree = renderer
-      .create(<MonthItem id={''} name={''} days={[]} />)
+      .create(
+        <Provider store={store}>
+          <MonthItem id={''} name={''} days={[]} />
+        </Provider>
+      )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
