c8a6a2547ce105ad06b746ca7b911b34ddbdcafa
WildPastry <mparkernzdev@gmail.com>
2024-02-26 19:22:32 +1300

    Adjust icons. Add test.

--- app/App.tsx
+++ app/App.tsx
@@ -1,8 +1,10 @@
+import { Text, View } from './components/styles/Themed';
 import { ColorSchemeName } from 'react-native';
 import Navigation from './navigation';
 import { Provider } from 'react-redux';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native';
 import { registerRootComponent } from 'expo';
 import store from './redux/store';
 import useCachedResources from './hooks/useCachedResources';
@@ -27,10 +29,27 @@ const App: React.FC = (): JSX.Element | null => {
   }
   return (
     <SafeAreaProvider>
-      <Navigation colorScheme={colorScheme} />
+      {/* <Navigation colorScheme={colorScheme} /> */}
+      <View style={styles.container}>
+        <Text style={styles.title}>JFT</Text>
+      </View>
       <StatusBar />
     </SafeAreaProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#445566',
+    flex: 1,
+    justifyContent: 'center',
+    padding: 15
+  },
+  title: {
+    color: 'white',
+    fontSize: 50
+  }
+});
 
 export default registerRootComponent(AppWithProvider);
