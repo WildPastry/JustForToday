26486ef1b15d7b5472b27479e2836e499bd44a6e
WildPastry <mparkernzdev@gmail.com>
2024-05-19 15:30:40 +1200

    Testing updates

--- app/App.tsx
+++ app/App.tsx
@@ -3,20 +3,21 @@ import 'expo-dev-client';
 import { ColorSchemeName } from 'react-native';
 import Navigation from './navigation';
 import { Provider } from 'react-redux';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { registerRootComponent } from 'expo';
 import store from './redux/store';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
+import { Text, View } from './components/styles/Themed';
 
-const AppWithProvider: React.FC = (): JSX.Element => {
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-};
+// const AppWithProvider: React.FC = (): JSX.Element => {
+//   return (
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   );
+// };
 
 const App: React.FC = (): JSX.Element | null => {
   // Colour settings
@@ -28,11 +29,18 @@ const App: React.FC = (): JSX.Element | null => {
     return null;
   }
   return (
-    <SafeAreaProvider>
-      <Navigation colorScheme={colorScheme} />
-      <StatusBar />
-    </SafeAreaProvider>
+    <View>
+      <Text>JFT</Text>
+      {/* <Navigation colorScheme={colorScheme} /> */}
+      {/* <StatusBar /> */}
+  </View>
+    // <SafeAreaProvider>
+    //   <SafeAreaView>
+    //     <Navigation colorScheme={colorScheme} />
+    //     <StatusBar />
+    //   </SafeAreaView>
+    // </SafeAreaProvider>
   );
 };
 
-export default registerRootComponent(AppWithProvider);
+export default registerRootComponent(App);
